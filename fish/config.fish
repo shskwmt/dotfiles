f2cb333449adb6caf8bcd112de31d3c6ffa4abc9
shskwmt <shskwmt@outlook.jp>
2024-06-20 15:31:29 +0900

    feat: add an alias bf

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,7 @@ function subcommand_abbr
 end
 
 alias cf='cd (find . -type d | fzf)'
+alias bf='bat (fzf)'
 
 for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.fish' -not -path '*.git*' -not -path '*fish/*.fish')
     source $src
