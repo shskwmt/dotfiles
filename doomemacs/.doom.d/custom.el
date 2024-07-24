8df8a7105a6440cf252ec5170d2e6b68ffcd0b69
shskwmt <shskwmt@outlook.jp>
2024-07-25 00:14:04 +0900

    feat: use theme doom-tokyonight

--- doomemacs/.doom.d/custom.el
+++ doomemacs/.doom.d/custom.el
@@ -4,6 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "4825b816a58680d1da5665f8776234d4aefce7908594bea75ec9d7e3dc429753" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
