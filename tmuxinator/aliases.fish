2fea33f9c6f6a60a41b2d018fb6c0b0918c958f4
shskwmt <shskwmt@outlook.jp>
2024-08-22 20:12:25 +0900

    feat: update tmuxinator settings

--- tmuxinator/aliases.fish
+++ tmuxinator/aliases.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
 alias mux='tmuxinator'
-alias ms='set wpath (fd --full-path ~ -t d | ft) && tmuxinator start project -n (echo $wpath | rev | cut -d "/" -f 2 | rev) "$HOME/$wpath"'
+alias ms='set wpath (fd -H --full-path ~ -t d | ft) && tmuxinator start project -n (echo $wpath | rev | cut -d "/" -f 2 | rev) "$HOME/$wpath"'
