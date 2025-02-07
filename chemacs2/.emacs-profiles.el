2437034bb110b6ab6ee172f50d13b02147020180
shskwmt <shskwmt@outlook.jp>
2025-02-08 00:21:26 +0900

    feat: delete chemacs2 and doomemacs

--- chemacs2/.emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.emacs.hikizan")))
- ("hikizan" . ((user-emacs-directory . "~/.emacs.hikizan")))
- ("sandbox" . ((user-emacs-directory . "~/.emacs.sandbox")))
- ("doom" . ((user-emacs-directory . "~/.config/emacs"))))
--- doomemacs/env.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-set -x PATH "~/.config/emacs/bin:$PATH"
-set -x DOOMDIR "~/.doom.d"
